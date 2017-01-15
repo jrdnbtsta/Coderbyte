c1f01ec1a424860c204c2c6de5977c0e1e7976f0
Jordan Bautista <jordanbau@gmail.com>
2017-01-15 14:06:37 -0800

    Solution for Time Converter

---
+++ timeConverter.js
@@ -0,0 +1,18 @@
+function CheckNums(num1,num2) { 
+    //if num2 is greater than num1, return true
+    if(num2>num1){
+        return true;
+    }else if (num1>num2){
+        return false;
+    }else{
+        return '-1';
+    }
+    //if num1 is greater than num2, return false
+    //otherwise, return '-1'
+  
+ 
+         
+}
+   
+// keep this function call here 
+CheckNums(readline()); 
