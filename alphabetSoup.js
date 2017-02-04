78b78efe1b890ccc66ad8589daf18ee935ff3fdc
Jordan Bautista <jordanbau@gmail.com>
2017-02-04 15:42:32 -0800

    Solution for Alphabet Soup -- Added prompt description

--- alphabetSoup.js
+++ alphabetSoup.js
@@ -1,7 +1,35 @@
-function AlphabetSoup(str) { 
+/***************************************************************************************
+*                                                                                      *
+*                  CODERBYTE BEGINNER CHALLENGE                                        *
+*                                                                                      *
+*  Alphabet Soup                                                                       *
+*  Have the function AlphabetSoup(str) take the str string parameter being passed      *
+*  and return the string with the letters in alphabetical order                        *
+*  (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be       *
+*  included in the string.                                                             *
+*                                                                                      *
+***************************************************************************************/
 
-    return str.split("").sort().join("");
-         
+/*
+Rewritten Prompt: We are given a string. return the string with each letter in
+alphabetical order
+
+Solution:
+1. split string into array
+2. sort array
+3. join array
+4. return array
+
+*/
+
+
+
+function AlphabetSoup(str){
+  
+  var array = str.split('').sort().join('')
+  
+  return array
+  
 }
 
-AlphabetSoup(readline());  
+console.log(AlphabetSoup("hello"))
