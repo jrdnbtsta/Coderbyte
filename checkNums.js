52ff187fc086ed575f12b2906d45914a26114d25
Jordan Bautista <jordanbau@gmail.com>
2017-02-05 10:16:08 -0800

    Solution for Check Nums -- added prompt & revised for clarity

--- checkNums.js
+++ checkNums.js
@@ -1,18 +1,33 @@
-function CheckNums(num1,num2) { 
-    //if num2 is greater than num1, return true
-    if(num2>num1){
-        return true;
-    }else if (num1>num2){
-        return false;
-    }else{
-        return '-1';
-    }
-    //if num1 is greater than num2, return false
-    //otherwise, return '-1'
+/***************************************************************************************
+*                                                                                      *
+*                  CODERBYTE BEGINNER CHALLENGE                                        *
+*                                                                                      *
+*                                                                                      *
+*  Using the JavaScript language, have the function CheckNums(num1,num2) take both     *
+*  parameters being passed and return the string true if num2 is greater than num1,    *
+*  otherwise return the string false. If the parameter values are equal to each other  *
+*  then return the string -1                                                           *
+*                                                                                      *
+***************************************************************************************/
+
+
+/*
+Rewritten Prompt: You are given two numbers. If num2 is greater than num1, return true.
+If num1 is greater, return false. If tey are equal, return -1.
+
+
+Solution: write conditional statements for each of the possible outcomes
+
+*/
+
+
+function CheckNums(num1,num2){
   
- 
-         
+  if(num2 > num1) return true;
+  if(num1> num2) return false
+  else return -1
+  
+  
 }
-   
-// keep this function call here 
-CheckNums(readline()); 
+
+console.log(CheckNums(7,6))
